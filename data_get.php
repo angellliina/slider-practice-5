4695346112bbcd5ada4e05c9c9dba86acbb0088d
angellliina <91912338+angellliina@users.noreply.github.com>
2021-10-18 23:25:40 +0300

    Add files via upload

---
+++ data_get.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data</title>
+</head>
+<body>
+<?php echo $_GET["name"]; ?><br>
+    <?php echo $_GET["birthday"]; ?><br>
+    <?php echo $_GET["email"]; ?><br>
+    <?php echo $_GET["gender"]; ?><br>
+    <?php echo $_GET["country"]; ?><br>
+    <?php echo $_GET["phone"]; ?><br>
+    <?php echo $_GET["pass"]; ?><br>
+    <?php echo $_GET["note"]; ?><br>
+</body>
+</html>
